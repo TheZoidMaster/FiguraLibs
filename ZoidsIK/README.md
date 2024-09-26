b8537589abdacc08fdf42fd0031555e93b0ccd84
Zoid <108158093+TheZoidMaster@users.noreply.github.com>
2024-09-26 17:16:56 -0500

    Updated Zoid's IK
    Added chain naming, luadocs, improved README, and updated the example avatar.

--- ZoidsIK/README.md
+++ ZoidsIK/README.md
@@ -1,26 +1,33 @@
 # Zoid's IK
+
 A simple but powerful FABRIK implementation, designed for ease of use.
 
 Forgive me if I've made a mistake in the documentation or the code, this is my first Figura library.
 
 # Docs
+
 ## Chain Class
-The chain class is used to create an IK chain. It must be created with 2 parameters. `parts` is an array of the Modelparts you wish to include in the chain. `lengths` is an array of the length of each part in the chain in blockbench units.
+
+The chain class is used to create an IK chain. It must be created with 3 parameters. `parts` is an array of the Modelparts you wish to include in the chain. `lengths` is an array of the length of each part in the chain in blockbench units. `name` is the name of the chain (used for sorting the modelparts).
 
 Example:
+
 ```lua
-local chain = ZoidsIK.Chain({
+local chain = Chain({
     parts = {
         models.model.segment1,
         models.model.segment2,
         models.model.segment3
     },
-    lengths = {10, 7, 5}
+    lengths = {10, 7, 5},
+    name = "example"
 })
 ```
+
 The chain can also be initialized with nested parts.
 
 Example:
+
 ```lua
 local chain = ZoidsIK.Chain({
     parts = {
@@ -28,49 +35,54 @@ local chain = ZoidsIK.Chain({
         models.model.segment1.segment2,
         models.model.segment1.segment2.segment3
     },
-    lengths = {10, 7, 5}
+    lengths = {10, 7, 5},
+    name = "example"
 })
 ```
+
 > [!IMPORTANT]  
-> In the model, the chain should be created pointing straight up or straight down, pointing in any other direction will most likely not work. Each segment in the chain should have its pivot centered.
+> In the model, the cubes/meshes should be oriented long way up. It does not matter which way the group is facing. Faliure to do so will result in inaccurate IK results.
 
 ## Variables
-Chains have 3 variables that can be modified:
 
-`rotation` - The rotation of each segment in the chain as a vector. (used for creating chains pointed in a direction other than up or down in the model — not recommended for now)
+Chains have 1 variable that can be modified:
 
-`rotationMultiplier` - The multiplier for the rotation of each segment in the chain. (used for creating chains pointed in a direction other than up or down in the model — not recommended for now)
-
-`autoPause` - If true, the chain will not solve if the target is out of range.
+`autoPause` - If true, the chain will not solve if the target is out of range. This is useful for chains that are constantly moving. Default is true.
 
 ## Methods
 
 `Chain:solve(root, target, iterations)` - Solves the chain. `root` is the world coordinates of the root of the chain. `target` is the world coordinates of the target of the chain. `iterations` is the number of iterations.
 
-`Chain:backward()` - Performs a backward pass on the chain.
+`Chain:backward()` - Performs a backward pass on the chain. Shouldn't be called manually except for special cases.
 
-`Chain:forward()` - Performs a forward pass on the chain.
+`Chain:forward()` - Performs a forward pass on the chain. Shouldn't be called manually except for special cases.
 
-`Chain:isInRange(pos)` - Returns true if the chain is in range of the target. If pos is nil, it will check if the current target position is in range instead.
+`Chain:isInRange(pos?)` - Returns true if the chain is in range of the target. If pos is not provided, it will check if the current target position is in range instead.
 
 # Usage
-## Code Example:
-```lua
-local ik = require("ZoidsIK")
 
-local chain = ik.Chain({
+## Code Example:
+
+```lua
+local Chain = require("ZoidsIK")
+
+local chain = Chain({
     parts = {
         models.path.to.part1,
         models.path.to.part2,
         models.path.to.part3
     },
-    lengths = {length1, length2, length3}
+    lengths = {length1, length2, length3},
+    name = "example"
 })
 
 function events.render()
-    chain:solve(vectors.vec3(0, 0, 0), vectors.vec3(0, 0, 0), 5)
+    chain:solve(vectors.vec3(0, 0, 0), vectors.vec3(1, 1, 1), 5)
 ```
+
 > [!TIP]  
-> Modelparts added to a chain will be moved to `models.ik_chain`, so make sure to use that when referencing them after initializing the chain.
-## Avatar Example:
+> Modelparts added to a chain will be moved to `models.ik.<name>` (e.g. `models.ik.example` for the example above), so make sure to use that when referencing them after initializing the chain.
+
+## Example Avatar:
+
 Download [Example.zip](https://github.com/TheZoidMaster/FiguraLibs/blob/main/ZoidsIK/Example.zip) and put it in your avatar folder, then equip the avatar and place your playerhead. It will create an IK chain that follows the nearest entity.
--- ZoidsIK/ZoidsIK.lua
+++ ZoidsIK/ZoidsIK.lua
@@ -1,82 +1,89 @@
-
-
 --[[
   _____     _     _     ___ _  __
  |__  /___ (_) __| |___|_ _| |/ /
-   / // _ \| |/ _` / __|| || ' / 
-  / /| (_) | | (_| \__ \| || . \ 
- /____\___/|_|\__,_|___/___|_|\_\                             
+   / // _ \| |/ _` / __|| || ' /
+  / /| (_) | | (_| \__ \| || . \
+ /____\___/|_|\__,_|___/___|_|\_\
 --]]
 
--- Zoid's IK implementation
--- https://github.com/TheZoidMaster/FiguraLibs/tree/main/ZoidsIK
+--- Zoid's IK implementation
+--- https://github.com/TheZoidMaster/FiguraLibs/tree/main/ZoidsIK
 
 
-
-local ZoidsIK = {}
-
-
-
--- Simple utility function to convert a direction vector to an angle vector
+--- Rotation helper function - thank you @4p5
+--- @param dir Vector3
 local function dirToAngle(dir)
-    return vec(math.deg(math.atan2(dir.y, math.sqrt(dir.x * dir.x + dir.z * dir.z))), math.deg(math.atan2(dir.x, dir.z)), 0)
+    return vec(math.deg(math.atan2(dir.y, math.sqrt(dir.x * dir.x + dir.z * dir.z))),
+        math.deg(math.atan2(dir.x, dir.z)), 0)
 end
 
-
--- Chain class
-ZoidsIK.Chain = {
+--- Chain class
+--- @class IKChain
+local IKChain = {
     parts = {},
-    lengths = {}
+    joints = {},
+    lengths = {},
+    totalLength = 0,
+    root = vectors.vec3(0, 0, 0),
+    target = vectors.vec3(0, 0, 0),
+    autoPause = true,
+    name = ""
 }
 
-
--- Chain constructor
-function ZoidsIK.Chain.new(parts, lengths)
+--- Creates a new chain.
+---
+--- All modelparts in the chain will be moved to `models.ik.<name>`.
+--- @param parts table
+--- @param lengths table
+--- @param name string
+--- @return IKChain
+function IKChain.new(parts, lengths, name)
     local self = {}
-    setmetatable(self, ZoidsIK.Chain)
+    setmetatable(self, { __index = IKChain })
     self.lengths = lengths
     self.totalLength = 0
     for i = 1, #lengths do
         self.totalLength = self.totalLength + lengths[i]
     end
     self.parts = parts
-    self.rotation = vectors.vec3(90,0,0)
-    self.rotationMultiplier = vectors.vec3(-1,1,1)
     self.joints = {}
-    self.root = vectors.vec3(0,0,0)
-    self.target = vectors.vec3(0,0,0)
+    self.root = vectors.vec3(0, 0, 0)
+    self.target = vectors.vec3(0, 0, 0)
     self.autoPause = true
+    self.name = name
 
-    -- Setup parts for proper positioning
     for i, part in ipairs(parts) do
         part:setParentType("WORLD")
-        part:setPos(vectors.vec3(0,0,0) - part:getPivot())
-        part:setPivot(0,0,0)
-        if not models.ik_chain then
-            _ = models:newPart("ik_chain")
+        part:setPos(vectors.vec3(0, 0, 0) - part:getPivot())
+        part:setPivot(0, 0, 0)
+        if not models.ik then
+            _ = models:newPart("ik")
         end
-        part:moveTo(models.ik_chain)
+        if not models.ik[name] then
+            _ = models.ik:newPart(name)
+        end
+        part:moveTo(models.ik[name])
         for _, child in ipairs(part:getChildren()) do
-            child:setPos(vectors.vec3(0,0,0) - child:getPivot())
-            child:setPivot(0,0,0)
+            child:setPos(vectors.vec3(0, 0, 0) - child:getPivot())
+            child:setPivot(0, 0, 0)
         end
     end
 
-    -- Create joints
     for i = 1, #parts + 1 do
-        self.joints[i] = vectors.vec3(0,0,0)
+        self.joints[i] = vectors.vec3(0, 0, 0)
     end
 
     return self
 end
 
-
--- Backward pass
-function ZoidsIK.Chain:backward(target)
-    -- Idk entirely how this works but it does so ¯\_(ツ)_/¯
+--- Performs a backward pass.
+---
+--- This shouldn't have to be called manually except in special cases.
+--- @param target Vector3
+function IKChain:backward(target)
     self.joints[#self.joints] = target
 
-    for i = #self.joints -1, 1, -1 do
+    for i = #self.joints - 1, 1, -1 do
         local r = (self.joints[i + 1] - self.joints[i])
         local l = self.lengths[i] / r:length()
 
@@ -85,10 +92,11 @@ function ZoidsIK.Chain:backward(target)
     end
 end
 
-
--- Forward pass
-function ZoidsIK.Chain:forward(root)
-    -- Idk entirely how this works but it does so ¯\_(ツ)_/¯
+--- Performs a forward pass.
+---
+--- This shouldn't have to be called manually except in special cases.
+--- @param root Vector3
+function IKChain:forward(root)
     self.joints[1] = root
 
     for i = 1, #self.joints - 1 do
@@ -100,50 +108,53 @@ function ZoidsIK.Chain:forward(root)
     end
 end
 
-
--- Solve chain
-function ZoidsIK.Chain:solve(root, target, iterations)
+--- Solves the chain.
+---
+--- If `Chain.autoPause` is `true`, it will only solve if the target is in reach.
+--- This is for performance reasons.
+--- @param root Vector3
+--- @param target Vector3
+--- @param iterations number
+function IKChain:solve(root, target, iterations)
     self.root = root
     self.target = target
 
-    -- If autoPause is enabled, don't solve if not in reach
-    -- This is for performance reasons and can be disabled
+
     if self.autoPause and not self:isInReach() then
         return
     end
 
-    -- Perform forward and backward passes for specified number of iterations
+
     for i = 1, iterations do
-        self:backward(target*16)
-        self:forward(root*16)
+        self:backward(target * 16)
+        self:forward(root * 16)
     end
 
 
-    -- Set positions and rotations of segments
     for i = 1, #self.lengths do
         local dir = self.joints[i + 1] - self.joints[i]
-        local pos = self.joints[i] + dir:normalized() * self.lengths[i]/2
+        local pos = self.joints[i] + dir:normalized() * self.lengths[i] / 2
 
         self.parts[i]:setPos(pos)
-        self.parts[i]:setRot(dirToAngle(dir):add(self.rotation):mul(self.rotationMultiplier))
+        self.parts[i]:setRot(dirToAngle(dir):add(vectors.vec3(90, 0, 0)):mul(vectors.vec3(-1, 1, 1)))
     end
 end
 
-
--- Check if target is in reach
-function ZoidsIK.Chain:isInReach(pos)
+--- Checks if a given position is in reach.
+---
+--- If `pos` is `nil`, it will use the IKChain's current target.
+--- @param pos? Vector3
+--- @return boolean
+function IKChain:isInReach(pos)
     if pos == nil then
         pos = self.target
     end
     local dist = (self.root - pos):length()
-    if dist > self.totalLength/16 then
+    print(self.totalLength, dist * 16)
+    if dist * 16 > self.totalLength + #self.lengths then
         return false
     end
     return true
 end
 
-function ZoidsIK.Chain.__index(t, k)
-    return ZoidsIK.Chain[k]
-end
-
-return ZoidsIK
+return IKChain
